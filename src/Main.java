6a90baf614417161180836aee291e9311c3b67e6
Sara <up202406368@up.pt>
2025-02-23 15:38:14 +0000

    Quinto commit - Versão final. Resolução da classe Main (exercício 2).

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Sara");
+        pessoa.setIdade(18);
+        pessoa.setAltura(1.64);
+        pessoa.setPeso(65.0);
+        System.out.println("Nome: " + pessoa.getNome());
+        System.out.println("Idade: " + pessoa.getIdade() + " anos");
+        System.out.println("Altura: " + pessoa.getAltura() + " metros");
+        System.out.println("Peso: " + pessoa.getPeso() + " kg");
+        System.out.println("IMC: " + pessoa.calcularIMC() + " kg/m2");
+        System.out.println("Classificação: " + pessoa.classificarIMC());
     }
 }
